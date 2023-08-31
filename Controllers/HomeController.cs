4df9c7d2d84e9037a4fe603791a4d1119a1757f0
vivianab45 <balleza45@gmail.com>
2023-08-31 14:39:10 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    static User NewUser;
+
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult FormView()
+    {
+        return View();
+    }
+
+    [HttpPost("process")]
+    public IActionResult Process(User user)
+    {
+        NewUser= user;
+        return RedirectToAction("ResultsView");
+    }
+
+    [HttpGet("results")]
+    public IActionResult ResultsView()
+    {
+        return View(NewUser);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+#pragma warning disable CS8618
+
+namespace DojoSurveyWithModel.Models;
+
+public class User
+{
+    public string Name {get;set;} 
+    public string Location {get;set;} 
+    public string Language {get;set;}  
+    public string Comment {get;set;} 
+}
